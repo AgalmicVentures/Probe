0bf1d987aac1215d89cc293336a5806a521acd20
Ian Hutchinson <ihutchinson@agalmicventures.com>
2015-03-07 20:55:48 -0600

    Stubbed JSON methods

--- probe/application/json_application.py
+++ probe/application/json_application.py
@@ -1,8 +1,30 @@
 
 import cherrypy
+import json
 
 class ProbeJsonApplication:
 
 	def __init__(self):
 		#TODO
 		pass
+
+	@cherrypy.expose
+	def hardware(self):
+		cherrypy.response.headers['Content-Type'] = 'application/json'
+		return json.dumps({
+			'TODO': True,
+		}).encode('utf8')
+
+	@cherrypy.expose
+	def software(self):
+		cherrypy.response.headers['Content-Type'] = 'application/json'
+		return json.dumps({
+			'TODO': True,
+		}).encode('utf8')
+
+	@cherrypy.expose
+	def status(self):
+		cherrypy.response.headers['Content-Type'] = 'application/json'
+		return json.dumps({
+			'TODO': True,
+		}).encode('utf8')
