bfafbf1a19884a1310394283a2242187df26b4f2
Ian Hutchinson <ihutchinson@agalmicventures.com>
2015-03-07 18:18:11 -0600

    Made a dynamically refreshable front page with Bootstrap

--- probe/application/__init__.py
+++ probe/application/__init__.py
@@ -1,5 +1,7 @@
 
 import cherrypy
+import io
+import socket
 
 import application.json_application
 import application.raw_application
@@ -9,11 +11,17 @@ class ProbeApplication:
 	def __init__(self):
 		self.json = json_application.ProbeJsonApplication()
 		self.raw = raw_application.ProbeRawApplication()
-		#TODO: human interface
+
+		with io.open('./templates/index.html') as indexTemplateFile:
+			self.indexTemplate = indexTemplateFile.read()
+
+		self.hostname = socket.gethostname()
 
 	@cherrypy.expose
 	def index(self):
-		return '<html><body><h1>Probe</h1></body></html>'
+		return self.indexTemplate % {
+			'hostname': self.hostname,
+		}
 
 	@cherrypy.expose
 	def default(self, *args, **kwargs):
