00e34a612136729232bbc0e46709275dc1c8adb2
Ian Hutchinson <ihutchinson@agalmicventures.com>
2015-03-12 18:10:46 -0500

    Implemented JSON API

--- probe/application/json_application.py
+++ probe/application/json_application.py
@@ -1,11 +1,12 @@
 
 import cherrypy
+from datetime import datetime
 import json
+import psutil
 
 class ProbeJsonApplication:
 
 	def __init__(self):
-		#TODO
 		pass
 
 	@cherrypy.expose
@@ -19,19 +20,48 @@ class ProbeJsonApplication:
 	def hardware(self):
 		cherrypy.response.headers['Content-Type'] = 'application/json'
 		return json.dumps({
-			'TODO': True,
-		}).encode('utf8')
-
-	@cherrypy.expose
-	def software(self):
-		cherrypy.response.headers['Content-Type'] = 'application/json'
-		return json.dumps({
-			'TODO': True,
-		}).encode('utf8')
+			'bootTime': datetime.fromtimestamp(psutil.boot_time()).strftime('%Y-%m-%d %H:%M:%S'),
+			'cpuCores': psutil.NUM_CPUS,
+			'ram': psutil.TOTAL_PHYMEM,
+		}, indent=4, sort_keys=True).encode('utf8')
 
 	@cherrypy.expose
 	def status(self):
+		bootTime = datetime.fromtimestamp(psutil.boot_time())
+		now = datetime.now()
+
+		virtualMemory = psutil.virtual_memory()
+		swapMemory = psutil.swap_memory()
+		network = psutil.net_io_counters()
+
 		cherrypy.response.headers['Content-Type'] = 'application/json'
 		return json.dumps({
-			'TODO': True,
-		}).encode('utf8')
+			'time': now.strftime('%Y-%m-%d %H:%M:%S'),
+			'uptime': int((now - bootTime).total_seconds()),
+			'cpuPercent': psutil.cpu_percent(interval=0.1),
+			'virtualMemory': {
+				'total': virtualMemory.total,
+				'available': virtualMemory.available,
+				'percent': virtualMemory.percent,
+				'used': virtualMemory.used,
+				'free': virtualMemory.free,
+			},
+			'swapMemory': {
+				'total': swapMemory.total,
+				'used': swapMemory.used,
+				'free': swapMemory.free,
+				'percent': swapMemory.percent,
+				'in': swapMemory.sin,
+				'out': swapMemory.sout,
+			},
+			'network': {
+				'bytesSent': network.bytes_sent,
+				'bytesReceived': network.bytes_recv,
+				'packetsSent': network.packets_sent,
+				'packetsReceived': network.packets_recv,
+				'errorIn': network.errin,
+				'errorOut': network.errout,
+				'droppedIn': network.dropin,
+				'droppedOut': network.dropout,
+			},
+		}, indent=4, sort_keys=True).encode('utf8')
