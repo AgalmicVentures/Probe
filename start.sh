a77991ff87cb22fd9eb0e504866a85917dbd3d4d
Ian Hutchinson <ihutchinson@agalmicventures.com>
2016-02-01 15:48:30 -0600

    Updated start/stop scripts for the new naming convention

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-PROCESSES=`ps xa | grep probe/__init__.py | grep -v grep`
+PROCESSES=`ps xa | grep Probe/__init__.py | grep -v grep`
 if [[ $? -eq 0 ]]; then
 	echo "Already running"
 	exit
 fi
 
 echo "Starting..."
-nohup ./probe/__init__.py &
+nohup python3 ./Probe/__init__.py &
 sleep 1
 echo "Started."
 
--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PROCESSES=`ps xa | grep probe/__init__.py | grep -v grep`
+PROCESSES=`ps xa | grep Probe/__init__.py | grep -v grep`
 if [[ $? -eq 1 ]]; then
 	echo "Not running"
 	exit
